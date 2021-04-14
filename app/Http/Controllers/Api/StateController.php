169c540ff7fc3a7e1b51e33cd194f5c03f88886e
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-14 00:45:15 -0300

    Implementa métodos de inserção, exibição, atualização e deleção no StateController

--- app/Http/Controllers/Api/StateController.php
+++ app/Http/Controllers/Api/StateController.php
@@ -16,21 +16,23 @@ class StateController extends Controller
 
     public function store(Request $request)
     {
-        //
+        State::create($request->all());
     }
     
     public function show($id)
     {
-        //
+        return State::findOrFail($id);
     }
     
     public function update(Request $request, $id)
     {
-        //
+        $state = State::findOrFail($id);
+        $state->update($request->all());
     }
 
     public function destroy($id)
     {
-        //
+        $state = State::findOrFail($id);
+        $state->delete();
     }
 }
