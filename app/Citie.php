456105ee6b2b75c6a1afe13e97a7e8c4d692c2ce
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-04-14 08:47:14 -0300

    Cria relacionamento da Cidade com Estado e atualiza migration de criação da tabela cities acrescentando nome e cahve de relacionamento com states

--- app/Citie.php
+++ app/Citie.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Citie extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    public function state() {
+        return $this->belongsTo('App\State');
+    }
 }
--- database/migrations/2021_04_14_023858_create_cities_table.php
+++ database/migrations/2021_04_14_023858_create_cities_table.php
@@ -15,6 +15,9 @@ class CreateCitiesTable extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->integer('state_id')->unsigned()->default(1);
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade');
             $table->timestamps();
         });
     }
